9ac54dc699f79e3c1d1ef0629aca7a63191afa43
i.tsaregorodtsev <tsaregorodtsevigorek@yandex.ru>
2025-10-25 23:34:46 +0400

    feat: add base structure

---
+++ src/windows/materials_window.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QMainWindow
+
+
+class MaterialsWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # TODO: Добавить ui и соответствующие методы
---
+++ src/windows/settings_window.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QMainWindow
+
+
+class SettingsWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # TODO: Добавить ui и соответствующие методы
